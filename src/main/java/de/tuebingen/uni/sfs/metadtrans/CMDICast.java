3987159bd00607937afa2c126f7531a1dabc3432
Kerl <nmeisinger@protonmail.com>
2021-06-17 01:03:46 +0200

    add ClasspathURIResolver for CMDI2HTML

--- src/main/java/de/tuebingen/uni/sfs/metadtrans/CMDICast.java
+++ src/main/java/de/tuebingen/uni/sfs/metadtrans/CMDICast.java
@@ -8,9 +8,7 @@ import javax.ws.rs.core.MediaType;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.xpath.XPath;
@@ -65,7 +63,8 @@ public class CMDICast {
 		return (is != null) ? is : cl.getResourceAsStream("../../" + filename);
 	}
 
-	public static File castFile(File dcfile, String converter) throws Exception {
+	public static File castFile(File dcfile, String converter) throws Exception
+	{
 		// String converter takes the convertion type as parameter (e.g. "CMDI2HTML"). Case does not matter
 
 		// standard call to xslt
@@ -77,14 +76,16 @@ public class CMDICast {
 		File output = null;
 
 		// XQuery Converter (CMDI2MARC and CMDI2DC)
-		if (converter.equalsIgnoreCase("CMDI2Marc") || converter.equalsIgnoreCase("CMDI2DC")) {
+		if (converter.equalsIgnoreCase("CMDI2Marc") || converter.equalsIgnoreCase("CMDI2DC"))
+		{
 			XQDataSource ds = new SaxonXQDataSource();
 			XQConnection conn = ds.getConnection();
 
 
-			try (   InputStream cmdi2marcStream      = getInputStream(converter.toLowerCase() + ".xquery");
-					InputStream cmdiInstanceStream = new FileInputStream(dcfile);
-					InputStream cmdiFileStream     = new FileInputStream(dcfile) ) {
+			try (InputStream cmdi2marcStream = getInputStream(converter.toLowerCase() + ".xquery");
+				 InputStream cmdiInstanceStream = new FileInputStream(dcfile);
+				 InputStream cmdiFileStream = new FileInputStream(dcfile))
+			{
 				// get the schemaLocation using xpath
 				XPathFactory xPathfactory = XPathFactory.newInstance();
 				XPath xpath = xPathfactory.newXPath();
@@ -107,15 +108,18 @@ public class CMDICast {
 				System.out.println("schema location found -" + schemaLocationString + "- in cmdi instance " + dcfile.getName());
 
 				// superfluous for NaLiDa-based profiles
-				if (schemaLocationString.endsWith("/xsd")) {
+				if (schemaLocationString.endsWith("/xsd"))
+				{
 					schemaLocationString = schemaLocationString.substring(0, schemaLocationString.length() - 4);
 				}
 
 				// In later versions of CMDI 1.2, we may need to update this (or use regular expression).
 				String registryPrefix = "";
-				if (CMDIVersionString.endsWith("1.2")) {
+				if (CMDIVersionString.endsWith("1.2"))
+				{
 					registryPrefix = "http://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/";
-				} else {
+				} else
+				{
 					// this is for CMDI 1.1 or earlier versions of CMDI (approximately: could match 1.3 or later)
 					registryPrefix = "http://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/profiles/";
 				}
@@ -136,9 +140,11 @@ public class CMDICast {
 				props.setProperty(OutputKeys.STANDALONE, "yes");
 				props.setProperty(OutputKeys.MEDIA_TYPE, MediaType.APPLICATION_XML);
 
-				if (converter.equalsIgnoreCase("CMDI2Marc")) {
+				if (converter.equalsIgnoreCase("CMDI2Marc"))
+				{
 					output = new File(dcfile.getParent(), name + ".marc.xml");
-				}else{
+				} else
+				{
 					output = new File(dcfile.getParent(), name + ".dc.xml");
 				}
 				System.out.println("TT");
@@ -149,8 +155,10 @@ public class CMDICast {
 			}
 		}
 		// XSL Converter (CMDi2HTMl, NaLiDa2Marc, Marc2RDFDC, Marc2EAD, DC2Marc)
-		else {
-			if (converter.equalsIgnoreCase("CMDI2HTML")) {
+		else
+		{
+			if (converter.equalsIgnoreCase("CMDI2HTML"))
+			{
 				clean_xsl = "CMDI2CMDI.xsl";
 				foo_xsl = "CMDI2HTML.xsl";
 
@@ -165,7 +173,8 @@ public class CMDICast {
 				dcfile = intermediate;
 				output = new File(dcfile.getParent(), name + ".html");
 
-			} else if (converter.equalsIgnoreCase("NaLiDa2Marc")) {
+			} else if (converter.equalsIgnoreCase("NaLiDa2Marc"))
+			{
 				clean_xsl = "CMDI2CMDI.xsl";
 				foo_xsl = "NaLiDa2MARC21_v1.2.xsl";
 				output = new File(dcfile.getParent(), name + ".marc21.xml");
@@ -181,16 +190,20 @@ public class CMDICast {
 				dcfile = intermediate;
 
 
-			} else if (converter.equalsIgnoreCase("Marc2RDFDC")) {
+			} else if (converter.equalsIgnoreCase("Marc2RDFDC"))
+			{
 				foo_xsl = "MARC21slim2RDFDC.xsl";
 				output = new File(dcfile.getParent(), name + ".rdfdc.xml");
-			} else if (converter.equalsIgnoreCase("Marc2MODS")) {
+			} else if (converter.equalsIgnoreCase("Marc2MODS"))
+			{
 				foo_xsl = "MARC21slim2MODS3-6.xsl";
 				output = new File(dcfile.getParent(), name + ".mods.xml");
-			} else if (converter.equalsIgnoreCase("Marc2EAD")) {
+			} else if (converter.equalsIgnoreCase("Marc2EAD"))
+			{
 				foo_xsl = "MARC21slim2EAD.xsl";
 				output = new File(dcfile.getParent(), name + ".ead.xml");
-			} else if (converter.equalsIgnoreCase("DC2Marc")) {
+			} else if (converter.equalsIgnoreCase("DC2Marc"))
+			{
 				foo_xsl = "DC2MARC21slim.xsl";
 				output = new File(dcfile.getParent(), name + ".marc21.xml");
 			}
@@ -199,10 +212,23 @@ public class CMDICast {
 
 			BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(output));
 			TransformerFactory tfactory = TransformerFactory.newInstance();
-			Transformer transformer = tfactory.newTransformer(new StreamSource(foo_xsl_stream)); // new File(foo_xsl)
+			StreamSource xslSource = new StreamSource(foo_xsl_stream);
+			System.out.println("Set StreamSource");
+			tfactory.setURIResolver(new ClasspathResourceURIResolver());
+			System.out.println("Set ClasspathResourceUIRResolver");
+			Templates cachedXSLT = tfactory.newTemplates(xslSource);
+			Transformer transformer = cachedXSLT.newTransformer(); // new File(foo_xsl)
 			transformer.transform(new StreamSource(dcfile),
 					new StreamResult(outputStream));
 		}
 		return output;
 	}
 }
+
+	class ClasspathResourceURIResolver implements URIResolver
+	{
+		@Override
+		public Source resolve(String href, String base) throws TransformerException {
+			return new StreamSource(this.getClass().getClassLoader().getResourceAsStream(href));
+		}
+	}
